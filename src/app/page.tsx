4b9a0f86012e1989f2a002591a3c64f11715c06f
sagar-shetty21 <5agarm5hetty05@gmail.com>
2025-01-28 17:17:36 +0530

    mobile nav bar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useScroll, useTransform, motion, MotionValue } from "framer-motion";
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
 
